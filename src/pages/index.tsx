8f06d57fb110460fe492f7894b0b85b1a1384b48
Felipe Gangrel <felipefrog@gmail.com>
2023-01-29 17:54:54 -0300

    Adds rocketseat eslint config and lints the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {Heading } from '@design-system-course/react'
+import { Heading } from '@design-system-course/react'
 
 export default function Home() {
   return (
@@ -8,7 +8,9 @@ export default function Home() {
         <title>Ignite Call Home</title>
       </Head>
       <div>
-        <Heading size="lg" as="h1">Hello world</Heading>
+        <Heading size="lg" as="h1">
+          Hello world
+        </Heading>
       </div>
     </>
   )
